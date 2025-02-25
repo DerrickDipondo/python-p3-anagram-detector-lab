20b4e19696a904a9f325a7862f173b5f5c97f7ab
DerrickDipondo <derrickdipondo@gmail.com>
2025-02-25 12:08:53 +0300

    Anagram Dector

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, anagrams):
+        # Sort letters of the word
+
+        sorted_word = sorted(self.word)
+
+        # Return  list of anagrams
+        return [anagram for anagram in anagrams if sorted(anagram.lower()) == sorted_word and anagram.lower() != self.word]
+    
